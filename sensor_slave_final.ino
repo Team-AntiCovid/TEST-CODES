49dce92098a823c3faa127a743db152e95004135
ADITYASTING <60431806+ADITYASTING@users.noreply.github.com>
2020-08-18 15:30:53 +0530

    Update sensor_slave_final.ino

--- sensor_slave_final.ino
+++ sensor_slave_final.ino
@@ -22,12 +22,12 @@ int SonarSensor( int trigpin, int vcc, int gnd, int echopin)
 void setup() {
   for (int thisPin = 4; thisPin < 8; thisPin++)
   {
-    digitalWrite(vcc, HIGH);
-    digitalWrite(gnd, LOW);
     pinMode(trigpin[thisPin], OUTPUT);
     pinMode(echopin[thisPin], INPUT);
     pinMode(vcc[thisPin], OUTPUT);
     pinMode(gnd[thisPin], OUTPUT);
+    digitalWrite(vcc, HIGH);
+    digitalWrite(gnd, LOW);
   }
 }
 void loop() {
